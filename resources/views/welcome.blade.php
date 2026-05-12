89be19f571b486137e9ab9b0bce9b9e5fe336e1f
Longsok <soklongyoung@gmail.com>
2026-05-12 19:25:45 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial scale=1.0">
+    <title>Laravel</title>
+    <link
+    href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.cs
+    s" rel="stylesheet">
+ </head>
+
+ <body class="bg-gray-100">
+    <div class="min-h-screen flex flex-col items-center justify-center p- 4">
+    <!-- Logo Section -->
+    <div class="text-center mb-8">
+    <img src="{{ asset('images/logo.jpg') }}" alt="Laravel Logo"
+    class="w-20 h-20 mx-auto">
+    <h1 class="text-4xl font-bold mt-4">Laravel</h1>
+    </div>
+    <!-- Your information -->
+    <div class="bg-white text-center p-6 rounded-lg shadow-md mb-6
+    w-full max-w-2xl">
+    <h2 class="text-2xl font-semibold mb-4">Your name: Sok
+    Dara</h2>
+    <h2 class="text-2xl font-semibold mb-4">Class: M1</h2>
+    </div>
+    <!-- Documentation Section -->
+    <div class="bg-white p-6 rounded-lg shadow-md mb-6 w-full maxw-2xl">
+    <h2 class="text-2xl font-semibold mb-4">Documentation</h2>
+    <p class="text-gray-700">
+    Laravel has wonderful documentation covering every
+    aspect of the framework. Whether you are a newcomer or have prior
+    experience with Laravel, we recommend reading our documentation from
+    beginning to end.
+    </p>
+    </div>
+    <!-- Laracasts Section -->
+    <div class="bg-white p-6 rounded-lg shadow-md mb-6 w-full maxw-2xl">
+    <h2 class="text-2xl font-semibold mb-4">Laracasts</h2>
+    <p class="text-gray-700">
+    Laracasts offers thousands of video tutorials on
+    Laravel, PHP, and JavaScript development. Check them out, see for
+    yourself, and massively level up your development skills in the process.
+    </p>
+    </div>
+    <!-- Laravel News Section -->
+    <div class="bg-white p-6 rounded-lg shadow-md mb-6 w-full maxw-2xl">
+    <h2 class="text-2xl font-semibold mb-4">Laravel News</h2>
+    <p class="text-gray-700">
+    Laravel News is a community-driven portal and
+    newsletter aggregating all of the latest and most important news in the
+    Laravel ecosystem, including new package releases and tutorials.
+    </p>
+    </div>
+    <!-- Vibrant Ecosystem Section -->
+ <div class="bg-white p-6 rounded-lg shadow-md w-full max-w-
+2xl">
+ <h2 class="text-2xl font-semibold mb-4">Vibrant Ecosystem</h2>
+ <p class="text-gray-700">
+ Laravel's robust library of first-party tools and
+libraries, such as Forge, Vapor, Nova, Envoyer, and Herd help you take
+your projects to the next level. Pair them with powerful open-source
+libraries like Cashier, Dusk, Echo, Horizon, Sanctum, Telescope, and more.
+ </p>
+ </div>
+ </div>
+ </body>
+ </html>
